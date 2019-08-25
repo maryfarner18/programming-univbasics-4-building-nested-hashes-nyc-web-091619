b7deb8955bde90f78dade8d8f03c005a15dd5ced
Mary Farner <maryfarner@gmail.com>
2019-08-25 14:12:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
 	      reading_railroad: {},
 	      b_and_o_railroad: {},
 	      pennsylvania_railroad: {},
-	      _railroad: {}
+	      shortline_railroad: {}
 	    }
 	    
 	  }	
