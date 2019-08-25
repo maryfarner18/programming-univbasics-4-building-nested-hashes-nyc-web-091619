98d0b91de8300b21009158577ff82f1fbae96c38
Mary Farner <maryfarner@gmail.com>
2019-08-25 14:15:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,13 +43,12 @@ def monopoly_with_fourth_tier
 	      two_pieces_owned: 50,
 	      three_pieces_owned: 100,
 	      four_pieces_owned: 200
-	    
 	    },
 	    names: {
 	      reading_railroad: { mortgage_value: "$100"},
-	      b_and_o_railroad: {mortgage_value: "$100"},
-	      pennsylvania_railroad: {mortgage_value: "$100"},
-	      shortline_railroad: {mortgage_value: "$100"}
+	      b_and_o_railroad: { mortgage_value: "$100"},
+	      pennsylvania_railroad: { mortgage_value: "$100"},
+	      shortline_railroad: { mortgage_value: "$100"}
 	    }
 	    
 	  }	
